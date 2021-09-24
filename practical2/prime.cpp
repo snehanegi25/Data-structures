dd0a070dff11267821ede2b7869303609f6a8d9f
snehanegi25 <91174457+snehanegi25@users.noreply.github.com>
2021-09-24 11:19:05 -0400

    Add files via upload

---
+++ practical2/prime.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int n, i;
+  bool isPrime = true;
+  cout << "Enter a positive integer: ";
+  cin >> n;
+  for(i = 2; i <= n / 2; ++i)
+  {
+      if(n % i == 0)
+      {
+          isPrime = false;
+          break;
+      }
+  }
+  if (isPrime)
+      cout << "prime number";
+  else
+      cout << "not a prime number";
+  return 0;
+}
